e0bcc4eb520819ce424ca07bc4ef685cfd84acb6
LanaStudio <196567654+vadim2807@users.noreply.github.com>
2025-09-28 21:58:51 +0300

    первая сдача

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,18 +1,19 @@
+import Link from 'next/link';
 import css from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={css.header}>
-      <a href="/" aria-label="Home">
+      <Link href="/" aria-label="Home">
         NoteHub
-      </a>
+      </Link>
       <nav aria-label="Main Navigation">
         <ul className={css.navigation}>
           <li>
-            <a href="/">Home</a>
+            <Link href="/">Home</Link>
           </li>
           <li>
-            <a href="/notes">Notes</a>
+            <Link href="/notes">Notes</Link>
           </li>
         </ul>
       </nav>
